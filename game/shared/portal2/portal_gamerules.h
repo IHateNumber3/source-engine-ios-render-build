6a73c6794f04ac6ff6b74b6330077abf29cf91e5
IHateNumber3 <lohudanynah@gmail.com>
2026-09-22 21:31:25 +0300

    Update portal_gamerules.h

--- game/shared/portal2/portal_gamerules.h
+++ game/shared/portal2/portal_gamerules.h
@@ -50,6 +50,7 @@ public:
 	
 	virtual bool	ShouldCollide( int collisionGroup0, int collisionGroup1 );
 	virtual bool	ShouldUseRobustRadiusDamage(CBaseEntity *pEntity);
+    void RegisterScriptFunctions( void );
 #ifndef CLIENT_DLL
 	virtual bool	ShouldAutoAim( CBasePlayer *pPlayer, edict_t *target );
 	virtual float	GetAutoAimScale( CBasePlayer *pPlayer );
@@ -112,8 +113,6 @@ inline CPortalGameRules* PortalGameRules()
 	return static_cast<CPortalGameRules*>(g_pGameRules);
 }
 
-
-void RegisterScriptFunctions( void );
 
 
 #endif // PORTAL_GAMERULES_H
