af8077e07cc91869920deb7a52b483d61a4ea622
IHateNumber3 <lohudanynah@gmail.com>
2026-09-23 11:06:35 +0300

    Update portal_gamerules.cpp

--- game/shared/portal2/portal_gamerules.cpp
+++ game/shared/portal2/portal_gamerules.cpp
@@ -1209,6 +1209,21 @@ bool CPortalGameRules::ShouldRemoveRadio( void )
 
 #endif//CLIENT_DLL
 
+void CPortalGameRules::RegisterScriptFunctions( void )
+{
+	if ( !g_pScriptVM )
+		return;
+
+#ifndef CLIENT_DLL
+	ScriptRegisterFunction( g_pScriptVM, IsMultiplayer, "Returns true if the game is running in multiplayer mode" );
+#endif
+}
+
+static bool IsMultiplayer( void )
+{
+	return ( gpGlobals->maxClients > 1 );
+}
+
 #ifdef CLIENT_DLL
 
 bool CPortalGameRules::IsBonusChallengeTimeBased( void )
@@ -1282,17 +1297,4 @@ CAmmoDef *GetAmmoDef()
 	return &def;
 }
 
-void CPortalGameRules::RegisterScriptFunctions( void )
-{
-	if ( !g_pScriptVM )
-		return;
 
-#ifndef CLIENT_DLL
-	ScriptRegisterFunction( g_pScriptVM, IsMultiplayer, "Returns true if the game is running in multiplayer mode" );
-#endif
-}
-
-static bool IsMultiplayer( void )
-{
-	return ( gpGlobals->maxClients > 1 );
-}
